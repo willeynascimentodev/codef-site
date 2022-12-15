55e6073914684444d22ece76677eefcb20f83254
Willey Nascimento <willeynascimentodev@waan.com.br>
2022-12-15 02:46:29 -0300

    Botão de expandir e colapsar no nome do produto

--- src/components/Produto.jsx
+++ src/components/Produto.jsx
@@ -1,15 +1,36 @@
 import Card from 'react-bootstrap/Card';
+import {  useState } from 'react';
 
 function Produto({img, name}) {
+
+    const [btnText, setBtnTxt] = useState('mais');
+    const [expanded, setExpanded] = useState(false);
+    const [height, setHeight] = useState('');
+    const [display, setDisplay] = useState('-webkit-box');
+
+
+    const expandCollapse = () => {
+        setExpanded(expanded ? false : true);
+        setBtnTxt(expanded ? "menos" : "mais" );
+        setHeight(expanded ? "auto" : "" );
+        setDisplay(expanded ? "" : "-webkit-box" );
+        console.log(expanded);
+    }
     
     return (
     <Card className='produto'>
     <Card.Img variant="top" src={img} />
-    <Card.Body>
-        <Card.Title style={{ 
-            
-         }}>
-            { name }
+    <Card.Body style={{ height: `${height}` }}>
+        <Card.Title>
+            <p className="text-elips" style={{ display: `${display}` }}>
+                { name } 
+            </p>
+            <span onClick={expandCollapse} className='text-elips-expand'>
+                {
+                    name.length >= 24 ? btnText : ''
+                }
+                
+            </span>
         </Card.Title>
     </Card.Body>
     </Card>
